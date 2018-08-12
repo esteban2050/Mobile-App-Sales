b40eecb0aa3ec4abd417c6708308448514f5ebff
esteban2050 <36140278+esteban2050@users.noreply.github.com>
2018-08-12 14:23:49 -0500

    Correcion del Modelo producto
    Se hace correcion debido a que se tenia la propiedad descripcion mal copiada.

--- Sales.Common/Models/Product.cs
+++ Sales.Common/Models/Product.cs
@@ -10,7 +10,7 @@
         public int ProductId { get; set; }
 
         [Required]
-        public string Desciption { get; set; }
+        public string Description { get; set; }
 
         public Decimal Price { get; set; }
 
