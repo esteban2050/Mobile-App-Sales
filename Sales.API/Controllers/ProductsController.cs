52d9e1942edd45c43f865f901dce4c3fbb92adef
esteban2050 <36140278+esteban2050@users.noreply.github.com>
2018-08-26 19:24:27 -0500

    Project
    Video # 21

--- Sales.API/Controllers/ProductsController.cs
+++ Sales.API/Controllers/ProductsController.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using Sales.Common.Models;
+using Sales.Domain.Models;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
-using Sales.Common.Models;
-using Sales.Domain.Models;
 
 namespace Sales.API.Controllers
 {
--- Sales/Sales/Helpers/Languages.cs
+++ Sales/Sales/Helpers/Languages.cs
@@ -17,50 +17,65 @@
         {
             get { return Resource.Accept; }
         }
+
         public static string Error
         {
             get { return Resource.Error; }
         }        
+
         public static string NoInternet
         {
             get { return Resource.NoInternet; }
         }
+
         public static string Products
         {
             get { return Resource.Products; }
         }
+
         public static string TurnOnInternet
         {
             get { return Resource.TurnOnInternet; }
         }
+
         public static string AddProduct
         {
             get { return Resource.AddProduct; }
         }
+
         public static string Description
         {
             get { return Resource.Description; }
         }
+
         public static string DescriptionPlaceholder
         {
             get { return Resource.DescriptionPlaceholder; }
         }
+
         public static string Price
         {
             get { return Resource.Price; }
         }
+
         public static string PricePlaceholder
         {
             get { return Resource.PricePlaceholder; }
         }
+
         public static string Remarks
         {
             get { return Resource.Remarks; }
         }
+
         public static string Save
         {
             get { return Resource.Save; }
         }
-
+        
+        public static string ChangeImage
+        {
+            get { return Resource.ChangeImage; }
+        }
     }
 }
